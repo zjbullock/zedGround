512ad301225eedfce9e15f6e22b886d64002245f
zjbullock <zjbulllock@gmail.com>
2019-10-14 18:18:00 -0400

    updated routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConwaysComponent } from './conways/conways.component';
@@ -38,7 +38,7 @@ import { ConwaysControlsComponent } from './conways-controls/conways-controls.co
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
